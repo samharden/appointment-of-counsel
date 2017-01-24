7701ec2aeda5dc4a8fe8b8b79cbb8dea19b9e0e5
samharden <samharden@gmail.com>
2017-01-23 23:48:49 -0500

    Update

--- main/form_choices.py
+++ main/form_choices.py
@@ -1,8 +1,8 @@
 CASETYPE_CHOICES = (
     ('crim', ("Criminal")),
-    ('small-claim', ("Small Claims")),
+    # ('small-claim', ("Small Claims")),
     ('discrim', ("Discrimination")),
-    ('mental', ("Mental Health")),
+    # ('mental', ("Mental Health")),
 
 )
 
