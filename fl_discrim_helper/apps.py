236c599eb66d671d5883cc0740d330e54e655e1d
samharden <samharden@gmail.com>
2017-07-31 14:43:44 -0400

    Second Commit

--- fl_discrim_helper/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FlDiscrimHelperConfig(AppConfig):
-    name = 'fl_discrim_helper'
--- small_claims/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SmallClaimsConfig(AppConfig):
-    name = 'small_claims'
